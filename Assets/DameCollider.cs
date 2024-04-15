cba2f7b090c4122f16f758f705d701d68fd33919
abigy <bilunvui1@gmail.com>
2024-04-15 17:18:02 +0700

    Sound burh

--- Assets/DameCollider.cs
+++ Assets/DameCollider.cs
@@ -8,9 +8,12 @@ public class DameCollider : MonoBehaviour
     Collider damageCollider;
     PlayerState playerState;
     public int currentHitDamage = 25;
-
+    private AudioSource hitAdudio;
+    public AudioClip[] hitAudioClip;
+    //public AudioClip hitAudioClip_2;
     private void Awake()
     {
+        hitAdudio = GetComponent<AudioSource>();
         playerState = GameObject.Find("Player").GetComponent<PlayerState>();
         damageCollider = GetComponent<Collider>();
         damageCollider.gameObject.SetActive(true);
@@ -46,6 +49,8 @@ public class DameCollider : MonoBehaviour
 
             if (enemyState != null)
             {
+                hitAdudio.clip = hitAudioClip[Random.Range(0, hitAudioClip.Length)];
+                hitAdudio.PlayOneShot(hitAdudio.clip, 1.0f);
                 playerState.recoverMana(15);
                 enemyState.TakeDame(currentHitDamage);
             }
